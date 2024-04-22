dbad61ebc0ce53678f16acdb5e6a83742ff28e78
Xury Greer <xury.greer@gmail.com>
2024-04-22 14:29:25 -0700

    Create copy_object_name_to_data.py

---
+++ copy_object_name_to_data.py
@@ -0,0 +1,51 @@
+bl_info = {
+    "name": "Copy Object Name to Data",
+    "author": "Xury Greer",
+    "version": (1, 0),
+    "blender": (2, 80, 0),
+    "location": "Outliner > Context Menu",
+    "description": "Rename object data name to match object name",
+    "warning": "",
+    "doc_url": "",
+    "category": "Utility",
+}
+
+import bpy
+from bpy.types import Operator
+
+
+class OBJECT_OT_copy_object_name_to_data(Operator):
+    """Rename the object data of each selected object to match the object name"""
+
+    bl_idname = "object.copy_object_name_to_data"
+    bl_label = "Copy Object Name to Data"
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+
+        for object in bpy.context.selected_ids:
+            # Get the object data if it exists (collections, and empty objects have no data).
+            data = getattr(object, "data", None)
+
+            if not data:
+                continue
+
+            data.name = object.name
+
+        return {"FINISHED"}
+
+
+def draw(self, context):
+    """Draw the operator as an option on a menu."""
+    layout = self.layout
+    layout.operator(OBJECT_OT_copy_object_name_to_data.bl_idname)
+
+
+def register():
+    bpy.utils.register_class(OBJECT_OT_copy_object_name_to_data)
+    bpy.types.OUTLINER_MT_object.append(draw)
+
+
+def unregister():
+    bpy.utils.unregister_class(OBJECT_OT_copy_object_name_to_data)
+    bpy.types.OUTLINER_MT_object.remove(draw)
